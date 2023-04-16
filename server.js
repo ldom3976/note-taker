8ebadc554d5b463e4165f0a273a1d54102925836
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-04-16 16:19:50 -0400

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 app.use(express.static("public"));
 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+  app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+  
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        id: uuid(),
+      };
+  
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+  
+          parsedNotes.push(newNote);
+  
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully posted note.')
+          );
+        }
+      });
+
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json("Couldn't post note");
+    }
+  });
+
+  app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT} 🚀`);
-  });
+  console.log(`App listening at http://localhost:${PORT} 🚀`);
+});
